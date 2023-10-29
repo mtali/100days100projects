431e688905a7056c50a3d6bddef99da931f9f6e7
mtali <emmanuelmtali@yahoo.com>
2023-10-29 14:54:06 +0300

    Parallax project setup

---
+++ Day002 - parallax-background/js/core.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_HEIGHT = canvas.height = 700;
+const CANVAS_WIDTH = canvas.width = 800;
+
+let gameSpeed = 5;
+
+
