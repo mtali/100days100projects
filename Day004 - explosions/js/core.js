fb6dc19d3da805cb51a46693becc507f214bea82
mtali <emmanuelmtali@yahoo.com>
2023-10-31 20:58:06 +0300

    Rename 🤙

---
+++ Day004 - explosions/js/core.js
@@ -0,0 +1,68 @@
+const canvas = document.querySelector("#canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGHT = canvas.height = 700;
+let canvasPosition = canvas.getBoundingClientRect();
+const explosions = [];
+
+
+class Explosion {
+    constructor(x, y) {
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth * 0.5;
+        this.height = this.spriteHeight * 0.5;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = "assets/boom.png";
+        this.frame = 0;
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = "assets/boom.wav";
+    }
+
+    update() {
+        if (this.frame === 0) this.sound.play();
+        this.timer++
+        if (this.timer % 10 === 0) {
+            this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.height / 2, this.width, this.height);
+        ctx.restore();
+    }
+}
+
+
+window.addEventListener('click', e => {
+    createAnimation(e);
+});
+
+function createAnimation(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+}
+
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate)
+}
+
+animate();
