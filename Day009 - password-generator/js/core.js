f257c1b77ae57b09243402fa7d1442189fec05f0
mtali <emmanuelmtali@yahoo.com>
2023-11-05 23:55:20 +0300

    Add function to generate password

--- Day009 - password-generator/js/core.js
+++ Day009 - password-generator/js/core.js
@@ -24,8 +24,41 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+const randomFunction = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
 
 
+generate.addEventListener('click', event => {
+    const length = +passwordLength.value;
+    const lower = includeLower.checked;
+    const upper = includeUpper.checked;
+    const number = includeNumbers.checked;
+    const symbol = includeSymbols.checked;
+
+    password.value = generatePassword(lower, upper, number, symbol, length);
+
+});
+
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) return '';
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const functionName = Object.keys(type)[0];
+            generatedPassword += randomFunction[functionName]();
+        })
+    }
+    return generatedPassword.slice(0, length);
+}
 
 
 
