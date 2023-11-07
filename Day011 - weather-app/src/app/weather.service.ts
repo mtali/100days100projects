966c64d97bd9e3d090dbb002c278f6a8b826983f
mtali <emmanuelmtali@yahoo.com>
2023-11-07 19:20:01 +0300

    Create a weather service

---
+++ Day011 - weather-app/src/app/weather.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor() {
+
+  }
+
+  getWeather() {
+    console.log("requesting weather");
+  }
+}
--- Day011 - weather-app/src/app/weather/weather.component.ts
+++ Day011 - weather-app/src/app/weather/weather.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "../weather.service";
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styles: []
 })
-export class WeatherComponent {
-  defaultIcon: string = "/assets/263.png"
+export class WeatherComponent implements OnInit {
+  defaultIcon: string = "/assets/263.png";
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.weatherService.getWeather();
+  }
 }
