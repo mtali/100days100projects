dd5dbb5e15eca39f8e4c4f5bedae0eb72228e6ed
mtali <emmanuelmtali@yahoo.com>
2023-11-18 02:49:11 +0300

    Refactor 🧑‍💻

--- Day021 - side-menu-animation/src/core.js
+++ Day021 - side-menu-animation/src/core.js
@@ -2,17 +2,6 @@ window.addEventListener('load', () => {
     const sidebar = document.getElementById('sidebar');
     const menu = document.getElementById('menu');
     const content = document.getElementById('content');
-    const languages = document.querySelectorAll('.language li');
-
-
-    languages.forEach(element => {
-        element.addEventListener('mouseover', e => {
-            e.target.classList.add('activated');
-        })
-        element.addEventListener('mouseout', e => {
-            e.target.classList.remove('activated');
-        })
-    });
 
     menu.addEventListener('click', () => {
         sidebar.classList.toggle('active');
