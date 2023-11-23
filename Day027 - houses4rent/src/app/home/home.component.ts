84df923fd9d7c3c6153cc1b57432c3c0db43f818
mtali <emmanuelmtali@yahoo.com>
2023-11-23 21:25:51 +0300

    Create housing location component and integrate it to home component

--- Day027 - houses4rent/src/app/home/home.component.ts
+++ Day027 - houses4rent/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ Day027 - houses4rent/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
