be6dd94b73e0f5a05400f9ad72745198504b4c0a
mtali <emmanuelmtali@yahoo.com>
2023-11-23 21:50:06 +0300

    Display home locations

--- Day027 - houses4rent/src/app/housing-location/housing-location.component.ts
+++ Day027 - houses4rent/src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HousingLocation} from "../housing-location";
 
 @Component({
   selector: 'app-housing-location',
@@ -9,5 +10,5 @@ import {CommonModule} from '@angular/common';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
