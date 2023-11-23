640e2766b3a15f4624f4dd67388204d4cadbc852
mtali <emmanuelmtali@yahoo.com>
2023-11-23 22:13:10 +0300

    Add details component and ability to route to it

--- Day027 - houses4rent/src/app/app.routes.ts
+++ Day027 - houses4rent/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {DetailsComponent} from "./details/details.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home'
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+    title: 'Details'
   }
 ];
