7c4b43245311fcb9886056e98d1b67dbf6b1b782
mtali <emmanuelmtali@yahoo.com>
2023-11-25 05:46:32 +0300

    Refactor code

--- Day028 - pulses/src/core.js
+++ Day028 - pulses/src/core.js
@@ -1,13 +1,23 @@
 const circle = document.querySelector('.circle');
 let count = 0;
 
-circle.textContent = `${count}s`;
+displayCount();
+
 
 function increment() {
     count++;
     if (count > 60) count = 0;
+    displayCount();
+}
+
+function displayCount() {
     circle.textContent = `${count}s`;
 }
+
+document.addEventListener('click', () => {
+    count = 0;
+    displayCount();
+});
 
 setInterval(() => {
     increment();
